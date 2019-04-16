548b5d404b69be8c0d68840b36c2fa56e572cd7f
Darshan <brunodarshan@gmail.com>
2019-04-16 12:28:43 -0300

    create views and templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def index(request):
+    template = loader.get_template('search.html')
+    return HttpResponse(template.render({},request))
+    
+
+def search(request, q):
+    if q != None:
+        return HttpResponse(q)
+    else:
+        return HttpResponse('Eita')
--- search_app/urls.py
+++ search_app/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views as coreViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', coreViews.index),
+    path('q=<str:q>', coreViews.search)
 ]
