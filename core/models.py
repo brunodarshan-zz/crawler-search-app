bd245e51df2cfbfdf89d4dd28cbe9a0154df8635
Darshan <brunodarshan@gmail.com>
2019-04-16 14:46:23 -0300

    add to string

--- core/models.py
+++ core/models.py
@@ -4,3 +4,6 @@ class Search(models.Model):
     source = models.CharField(max_length=300)
     query = models.CharField(max_length=120)
     deep = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.query + " was searched on " + self.source
